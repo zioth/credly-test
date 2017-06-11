49eec13c1e1baae9dc7ae1076ca67c80db524470
Zioth <junk1@zioth.com>
2017-06-11 09:28:24 -0400

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,67 +7,11 @@
 
 		<title>Eli's Credly App</title>
 
-		<!-- Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 		<!--
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="/css/app.css">
 		-->
-
-		<!-- Styles -->
-		<style>
-			html, body {
-				background-color: #fff;
-				color: #636b6f;
-				font-family: 'Raleway', sans-serif;
-				font-weight: 100;
-				height: 100vh;
-				margin: 0;
-			}
-
-			.full-height {
-				height: 100vh;
-			}
-
-			.flex-center {
-				align-items: center;
-				display: flex;
-				justify-content: center;
-			}
-
-			.position-ref {
-				position: relative;
-			}
-
-			.top-right {
-				position: absolute;
-				right: 10px;
-				top: 18px;
-			}
-
-			.content {
-				text-align: center;
-			}
-
-			.title {
-				font-size: 84px;
-			}
-
-			.links > a {
-				color: #636b6f;
-				padding: 0 25px;
-				font-size: 12px;
-				font-weight: 600;
-				letter-spacing: .1rem;
-				text-decoration: none;
-				text-transform: uppercase;
-			}
-
-			.m-b-md {
-				margin-bottom: 30px;
-			}
-		</style>
 	</head>
 	<body ng-controller="BadgesController as badgesController">
 		<div class="main-wrapper container">
@@ -108,6 +52,8 @@
 
 
 		<div class="flex-center position-ref full-height">
+			<!-- TODO Authentication -->
+<!--
 			@if (Route::has('login'))
 				<div class="top-right links">
 					@if (Auth::check())
@@ -118,12 +64,7 @@
 					@endif
 				</div>
 			@endif
-
-			<div class="content">
-				<div class="title m-b-md">
-					Laravel
-				</div>
-			</div>
+-->
 		</div>
 
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
