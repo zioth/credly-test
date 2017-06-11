97750e0c55155507d48701a7902e1db31be0b7f8
Zioth <junk1@zioth.com>
2017-06-11 09:28:51 -0400

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,17 +21,6 @@
 				event-name="loadMoreBadges"
 				ng-class="{'loading': badgesController.isLoading}"
 				>
-				<a
-					class="badge-image"
-					href="https://credly.com/recipients/{{ badge.id }}"
-					target="_blank"
-					ng-repeat="badge in badgesController.badges">
-					<div class="clearfix" ng-if="$index % 3 == 0"></div>
-					<div class="col-xs-4">
-						<div>{{ badge.title }}</div>
-						<img ng-src="{{ badge.image_url | imageFilter: 13 }}" />
-					</div>
-				</a>
 				<div class="loader loader--style2" title="1" ng-show="badgesController.isLoading">
 				  <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 					 width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
