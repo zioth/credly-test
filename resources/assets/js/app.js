47855e643e37fdb09a47ea61946906113cfa7f35
Zioth <junk1@zioth.com>
2017-06-11 10:30:21 -0400

    .

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,11 +3,9 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-/*
 require('./bootstrap');
 
-window.Vue = require('vue');
-*/
+//window.Vue = require('vue');
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
