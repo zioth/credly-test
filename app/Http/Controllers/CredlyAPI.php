719170cec5c9b0a9809ac9d94e7379ed9bf79531
Zioth <junk1@zioth.com>
2017-06-11 15:53:14 -0400

    .

--- app/Http/Controllers/CredlyAPI.php
+++ app/Http/Controllers/CredlyAPI.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
-use Illuminate\Cookie;
+use Illuminate\Support\Facades\Cookie;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
