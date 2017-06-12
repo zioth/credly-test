9aa089a48dca2f616dbc419701410583a97a59b7
Zioth <junk1@zioth.com>
2017-06-12 01:37:35 -0400

    .

--- app/Http/Controllers/CredlyAPI.php
+++ app/Http/Controllers/CredlyAPI.php
@@ -82,13 +82,13 @@ class CredlyAPI extends Controller {
 		$curl = curl_init();
 
 		curl_setopt_array($curl, [
-			CURLOPT_URL => "https://api.credly.com/v1.1/authenticate",
+			CURLOPT_URL => $url,
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_ENCODING => '',
 			CURLOPT_MAXREDIRS => 10,
 			CURLOPT_TIMEOUT => 30,
 			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_CUSTOMREQUEST => $method,
 			CURLOPT_HTTPHEADER => [
 				'x-api-key: ' . env('CREDLY_API_KEY'),
 				'x-api-secret: ' . env('CREDLY_API_SECRET')
