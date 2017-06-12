5756847c525e9666235386588792757ef53a7e30
Zioth <junk1@zioth.com>
2017-06-11 22:31:57 -0400

    .

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-use App\Http\Controllers;
+use App\Http\Controllers\CredlyAPI;
 
 Route::get('/', function () {
 	return view('mainapp');
